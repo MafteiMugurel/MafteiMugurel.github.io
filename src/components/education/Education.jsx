48486be7472e02266a2da8463f432506daf9b21b
MafteiMugurel <mugurel.maftei95@gmail.com>
2022-08-31 01:45:00 +0300

    style for education

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -10,20 +10,19 @@ const Education = () => {
         <div className="education-content-school">
           <img src={SCOALA_INFORMALA_DE_IT} alt="ScoalaInformaladeIT" />
           <div className="education-content-school-text">
-            Scoala Informala IT Frontend Developer, Computer Software
-            EngineeringFrontend Developer, Computer Software Engineering May
-            2022 - Aug 2022May 2022 - Aug 2022 The course includes the following
-            chapters: - GIT and Visual Studio Code; - HTML and CSS; - Responsive
-            design; - Javascript; - AJAX and Promises; - OOP; - ES6; - BOM; -
-            React; - A final project containing all the technologies learned.
+            <div>Scoala Informala de IT</div>
+            <div>Professional training course</div>
+            <div>Frontend Development</div>
+            <div>May 2022 - Sep 2022</div>
           </div>
         </div>
         <div className="education-content-school">
           <img src={APOLLONIA} alt="Apollonia" />
           <div className="education-content-school-text">
-            Universitatea „Apollonia” din Iași Bachelor's degree, Medicinal and
-            Pharmaceutical ChemistryBachelor's degree, Medicinal and
-            Pharmaceutical Chemistry 2015 - 2018 2015 - 2018 Grade: 9.10
+            <div>Universitatea „Apollonia”</div>
+            <div>Bachelor's degree</div>
+            <div>Dental Technique</div>
+            <div>2015 - 2018</div>
           </div>
         </div>
       </div>
